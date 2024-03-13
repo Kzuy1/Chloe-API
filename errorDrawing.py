7cfabab2a3d39d37bc6bec3f526afa84b729eee8
Kzuy1 <leoczuy@gmail.com>
2024-03-13 16:52:07 -0300

    Update for add new Verify Error Drawing

--- errorDrawing.py
+++ errorDrawing.py
@@ -4,10 +4,16 @@ class errorDrawing:
         self.ed02 = {'description': ':lady_beetle: [Error ED02:](https://discord.com/channels/1122685290205679748/1207328035108298804/1207339702152200215) Bloco de Legenda está com código errado.','booleanValue': False }
         self.ed03 = {'description': ':lady_beetle: [Error ED03:](https://discord.com/channels/1122685290205679748/1207328035108298804/1207342449450553445) Bloco de Legenda está com SCALA errado.','booleanValue': False }
         self.ed04 = {'description': ':lady_beetle: [Error ED04:](https://discord.com/channels/1122685290205679748/1207328035108298804/1207343645406003311) Bloco de Legenda está com DRAW, CHECKED ou APPROVED errado.','booleanValue': False }
-        self.ed05 = {'description': ':lady_beetle: [Error ED05:](https://discord.com/channels/1122685290205679748/1207328035108298804/1207355422072377344) Bloco de Revisão está com escala errado.','booleanValue': False }
+        # self.ed05 = {'description': ':lady_beetle: [Error ED05:](https://discord.com/channels/1122685290205679748/1207328035108298804/1207355422072377344) Bloco de Revisão está com escala errado.','booleanValue': False }
         self.ed06 = {'description': ':lady_beetle: [Error ED06:](https://discord.com/channels/1122685290205679748/1207328035108298804/1207356045186301983) LTScale está errado.','booleanValue': False }
-        self.ed07 = {'description': ':lady_beetle: [Error ED07:](https://discord.com/channels/1122685290205679748/1207328035108298804/1207358295304708148) Layer CONTOUR EXI está presente no Desenho','booleanValue': False }
-        self.ed08 = {'description': ':lady_beetle: [Error ED08:]() Linha de Chamada fora da Layer QUOTE','booleanValue': False }
+        self.ed07 = {'description': ':lady_beetle: [Error ED07:](https://discord.com/channels/1122685290205679748/1207328035108298804/1207358295304708148) Layer CONTOUR EXI está presente no Desenho.','booleanValue': False }
+        self.ed08 = {'description': ':lady_beetle: [Error ED08:](https://discord.com/channels/1122685290205679748/1207328035108298804/1217544751272693860) Linha de Chamada não está na camada QUOTE.','booleanValue': False }
+        self.ed09 = {'description': ':lady_beetle: [Error ED09:](https://discord.com/channels/1122685290205679748/1207328035108298804/1217546052631003216) Deve ter apenas um Bloco de Legenda no mesmo desenho.','booleanValue': False }
+        self.ed10 = {'description': ':lady_beetle: [Error ED10:](https://discord.com/channels/1122685290205679748/1207328035108298804/1217547272284274749) Bloco de Revisão 0 está com a Data diferente da Data de Emissão no Bloco de Legenda.','booleanValue': False }
+        self.ed11 = {'description': ':lady_beetle: [Error ED11:](https://discord.com/channels/1122685290205679748/1207328035108298804/1217552948385550427) Bloco de Revisão atual está diferente da Data de Revisão no Bloco de Legenda.','booleanValue': False }
+        self.ed12 = {'description': ':lady_beetle: [Error ED12:](https://discord.com/channels/1122685290205679748/1207328035108298804/1217555347217186969) Bloco de Revisão atual não preenchido.','booleanValue': False }
+        self.ed13 = {'description': ':lady_beetle: [Error ED13:](https://discord.com/channels/1122685290205679748/1207328035108298804/1217557431421501521) Bloco de Legenda não está com data de ','booleanValue': False }
+        self.edSC = {'description': ':lady_beetle: [Error EDSB:](https://discord.com/channels/1122685290205679748/1207328035108298804/1217558592119378030) Lista de Blocos na escala errada:\n','booleanValue': False }
         self.edOB = {'description': ':lady_beetle: [Error EDOB:](https://discord.com/channels/1122685290205679748/1207328035108298804/1207359201446207538) Lista de Blocos na versão antiga:\n','booleanValue': False }
 
     # self.edXX = {'description': ':lady_beetle: [Error EDXX:]() XXXXXXXXX','booleanValue': False }
--- verifyDXF.py
+++ verifyDXF.py
@@ -1,20 +1,31 @@
 from errorDrawing import errorDrawing
+from datetime import datetime
 import ezdxf
 import os
 import re
 
-class verifyDrawingDXF:
-    def __init__(self, fullPath):
+class Drawing:
+    def __init__(self, fullPath, dataIssue = None):
         self.errorDrawing = errorDrawing()
         self.fullPath = fullPath
+        self.dataIssue = dataIssue
         self.fileDrawingCode = self.getDrawingCode()
         self.fileDrawingCodeSeparate = self.getDrawingCodeSeparate()
         self.docDXF = ezdxf.readfile(self.fullPath)
         self.mspDXF = self.docDXF.modelspace()
-        self.subtitleBlock = self.getSubtitleBlock()
-        self.revisionBlocks = self.getBlockInfo("REDE-DISTINTA-REVISIONE")
-        self.partBlocks = self.getBlockInfo('ABC')
+        self.subtitleBlock = self.getBlockInfo('REDECAM-TITOLO-TAVOLA')
+        self.revisionBlocks = self.getBlockInfo('REDE-DISTINTA-REVISIONE')
+        self.revisionBlocks = self.sortBlock(self.revisionBlocks, 'REV-N')
 
+        #Verifica se existe dois ou mais Bloco de Título no mesmo Desenho, 
+        if len(self.subtitleBlock) != 1:
+            self.errorDrawing.ed09['booleanValue'] = True
+            self.message = self.errorDrawing.getErrorMessages()
+            return
+        # Se não transforma self.subtitleBlock em um só objeto invés de lista
+        self.subtitleBlock = self.subtitleBlock[0]
+        
+        self.checkdataIssue()
         self.checkCorrectSeparation()
         self.checksubtitleBlock()
         self.checkRevisionBlock()
@@ -28,9 +39,11 @@ class verifyDrawingDXF:
     # Função para pegar o código do Desenho
     def getDrawingCode(self):
         drawingCode = os.path.splitext(os.path.basename(self.fullPath))[0]
+        drawingCode = drawingCode.split('_', 1)
+        drawingCode = drawingCode[1]
         drawingCode = drawingCode.replace('EXECUTANDO_', '')
         drawingCode = drawingCode.replace('ENTREGA_', '')
-
+        
         return drawingCode
     
     # Função para pegar o código do Desenho separado
@@ -41,12 +54,18 @@ class verifyDrawingDXF:
 
         return codeSeparate
     
+    # Função para verificar a Data de Emissão
+    def checkdataIssue(self):
+        if self.dataIssue is None:
+            self.dataIssue = datetime.now().strftime("%d/%m/%y")
+
     # Função para pegar as informações de Blocos
     def getBlockInfo(self, blockName):
         blocksList = []
 
-        for insert in self.mspDXF.query('INSERT[name=="{blockName}"]'):
+        for insert in self.mspDXF.query(f'INSERT[name=="{blockName}"]'):
             blockInfo = {}
+            
             for attrib in insert.attribs:
                 tag = attrib.dxf.tag
                 value = attrib.dxf.text
@@ -60,43 +79,10 @@ class verifyDrawingDXF:
             blocksList.append(blockInfo)
         
         return blocksList
-
-    # Função para pegar as informações do Bloco de Título do Desenho
-    def getSubtitleBlock(self):
-        subtitleBlock = {}
-        for insert in self.mspDXF.query('INSERT[name=="REDECAM-TITOLO-TAVOLA"]'):
-            for attrib in insert.attribs:
-                tag = attrib.dxf.tag
-                value = attrib.dxf.text
-                height = attrib.dxf.height
-
-                subtitleBlock[tag] = {'value': value, 'height': height}
-            subtitleBlock['x_scale'] = insert.get_dxf_attrib('xscale') or 1
-            subtitleBlock['y_scale'] = insert.get_dxf_attrib('yscale') or 1
-            subtitleBlock['z_scale'] = insert.get_dxf_attrib('zscale') or 1
-
-        return subtitleBlock
     
-    # Função para pegar as informações do Bloco de Revisão
-    # def getRevisionBlock(self):
-    #     revisionBlocks = []
-    #     for insert in self.mspDXF.query('INSERT[name=="REDE-DISTINTA-REVISIONE"]'):
-    #         revisionBlock = {}
-    #         for attrib in insert.attribs:
-    #             tag = attrib.dxf.tag
-    #             value = attrib.dxf.text
-
-    #             revisionBlock[tag] = value
-    #         revisionBlock['x_scale'] = insert.get_dxf_attrib('xscale') or 1
-    #         revisionBlock['y_scale'] = insert.get_dxf_attrib('yscale') or 1
-    #         revisionBlock['z_scale'] = insert.get_dxf_attrib('zscale') or 1
-
-    #         revisionBlocks.append(revisionBlock)
-        
-    #     # Ordenar a lista de blocos de revisão com base no valor 'REV-N' em ordem ascendente
-    #     revisionBlocks = sorted(revisionBlocks, key=lambda x: int(x['REV-N']))
-        
-    #     return revisionBlocks
+    # Função para ordenar em forma crescente os Blocos de acordo com atributos
+    def sortBlock(self, blocksList, attribute):
+        return sorted(blocksList, key=lambda x: int(x[attribute]['value']))
 
     # Função para verificar se está separado certo o codigo
     def checkCorrectSeparation(self):
@@ -151,16 +137,36 @@ class verifyDrawingDXF:
         if 'S-CONT' in self.subtitleBlock and self.subtitleBlock['S-CONT']['value'] != 'VOL' :
             self.errorDrawing.ed04['booleanValue'] = True
 
-        # Verifica se o desenho foi aprovado por VOL
+        # Verifica se a aprovação está vazia, por que cliente deve aprovar
         if 'APPROVATO' in self.subtitleBlock and self.subtitleBlock['APPROVATO']['value'] != '' :
             self.errorDrawing.ed04['booleanValue'] = True
 
     # Função para verificar Escala dos Blocos de Revisão
     def checkRevisionBlock(self):
         for revisionBlock in self.revisionBlocks:
-
+            # Verifica Escala do Bloco de Revisão
             if abs(self.subtitleBlock['x_scale'] - revisionBlock['x_scale']) > 0.0001 :
-                self.errorDrawing.ed05['booleanValue'] = True
+                self.errorDrawing.edSC['booleanValue'] = True
+                self.errorDrawing.edSC['description'] += '\t\t REDE-DISTINTA-REVISIONE - Bloco de Revisão ' + revisionBlock['REV-N']['value']
+            
+            # Verifica Bloco de Revisão até última Revisão se está Preenchido
+            if int(revisionBlock['REV-N']['value']) <= int(self.fileDrawingCodeSeparate[5]):
+                for attribs in revisionBlock.values():
+                    if isinstance(attribs, dict) and attribs['value'] == None:
+                        self.errorDrawing.ed12['booleanValue'] = True
+            
+        # Verifica se a data Bloco de Revisão 0 é o mesmo no Bloco de Legenda
+        if self.revisionBlocks[0]['REV-D']['value'] != self.subtitleBlock['DATA']['value']:
+            self.errorDrawing.ed10['booleanValue'] = True
+        
+        # Verifica se a data da revisão atual do desenho condiz com o bloco de revisão
+        if self.revisionBlocks[int(self.fileDrawingCodeSeparate[5])]['REV-D']['value'] != self.subtitleBlock['D-REV']['value']:
+            self.errorDrawing.ed11['booleanValue'] = True
+        
+        # Verifica se a data da revisão atual do desenho condiz com a Date de Emissão do Usuário, Padrão: Data de Hoje
+        if self.revisionBlocks[int(self.fileDrawingCodeSeparate[5])]['REV-D']['value'] != self.dataIssue:
+            self.errorDrawing.ed13['booleanValue'] = True
+            self.errorDrawing.ed13['description'] += self.dataIssue
 
     # Função para verificar o LTScale
     def checkLineScaleFactor(self):
@@ -174,6 +180,8 @@ class verifyDrawingDXF:
         for leader in self.mspDXF.query('LEADER'):
             if leader.dxf.layer != 'QUOTE':
                 self.errorDrawing.ed08['booleanValue'] = True
+
+                return
 
     # Função para verificar se um bloco existe no Desenho
     def _checkBlockExists(self, blockName):
@@ -207,19 +215,32 @@ class verifyDrawingDXF:
                 self.errorDrawing.edOB['booleanValue'] = True
                 self.errorDrawing.edOB['description'] += '\t\t' + blockName[0] + " - " + blockName[1]
         
-        # Verificação de blocos de Solda
+        # Verificação de Blocos de Solda
         if self._checkBlockExists('TIPICO-SALDATURA_ENG-POR') or \
             self._checkBlockExists('TIPICO-SALDATURA_ITA-ENG'): 
                 self.errorDrawing.edOB['booleanValue'] = True
                 self.errorDrawing.edOB['description'] += '\t\tTIPICO-SALDATURA - Bloco de Solda\n'
 
-        # Verificação de blocos de formato
+        # Verificação de Blocos do Formato
         if self._checkBlockExists('REDE-A0') or \
             self._checkBlockExists('REDE-A1') or \
             self._checkBlockExists('REDE-A2') or \
             self._checkBlockExists('REDE-A3'):
                 self.errorDrawing.edOB['booleanValue'] = True
                 self.errorDrawing.edOB['description'] += '\t\tREDE - Bloco de folha\n'
+            
+        # Verificação do Bloco de Junta
+        if self._checkBlockExists('REDECAM-GUARNIZIONI_monolingua'):
+            block = self.docDXF.blocks.get('REDECAM-GUARNIZIONI_monolingua')
+
+            # Acessa as entidades dentro do bloco e procure um Texto escrito m (Metro)
+            for entity in block:
+                if entity.dxftype() == 'TEXT' and entity.dxf.text == 'm':
+                    return
+            
+            # Caso não tenha informa que o Bloco está na Versão antiga
+            self.errorDrawing.edOB['booleanValue'] = True
+            self.errorDrawing.edOB['description'] += '\t\tREDECAM-GUARNIZIONI - Bloco de Junta\n'
 
     # Função para verificar se a Layer Existe
     def checkLayerInR16(self):
