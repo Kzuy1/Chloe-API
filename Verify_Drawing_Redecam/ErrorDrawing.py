669bf2de4c485b908e7c40a906db2d5682ffa615
Kzuy1 <leoczuy@gmail.com>
2026-04-27 21:38:50 -0300

    feat: add `check_blocks_scale`

--- Verify_Drawing_Redecam/ErrorDrawing.py
+++ Verify_Drawing_Redecam/ErrorDrawing.py
@@ -25,7 +25,7 @@ class ErrorDrawing:
         self.er23 = {'description': ':lady_beetle: Error 23: Fator de Escala ou Nome da cota incorreta:\n','boolean_value': False }
         self.er24 = {'description': ':lady_beetle: Error 24: Bloco de Formato não está na origem (0,0,0).','boolean_value': False }
         self.er25 = {'description': ':lady_beetle: Error 25: Cota com Passo x Quantidade divergente da Dimensão.','boolean_value': False }
-        # self.er26 = {'description': ':lady_beetle: Error 26: Lista de Blocos na escala errada:\n','boolean_value': False }
+        self.er26 = {'description': ':lady_beetle: Error 26: Lista de Blocos na escala errada:\n','boolean_value': False }
         self.er27 = {'description': ':lady_beetle: Error 27: Lista de Blocos na versão antiga:\n','boolean_value': False }
 
     # self.erXX = {'description': ':lady_beetle: Error XX: XXXXXXXXX','boolean_value': False }
