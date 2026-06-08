4497a02c56afb6269bfce9d7d7e617a569c70f8d
Kzuy1 <leoczuy@gmail.com>
2026-06-08 20:54:36 -0300

    feat: add verify for title block description `TIT-2` and `TIT-4`

--- Verify_Drawing_Redecam/ErrorDrawing.py
+++ Verify_Drawing_Redecam/ErrorDrawing.py
@@ -28,6 +28,7 @@ class ErrorDrawing:
         self.er26 = {'description': ':lady_beetle: Error 26: Lista de Blocos na escala errada:\n','boolean_value': False }
         self.er27 = {'description': ':lady_beetle: Error 27: Lista de Blocos na versão antiga:\n','boolean_value': False }
         self.er28 = {'description': ':lady_beetle: Error 28: Marcas encontradas no desenho, mas ausente na Lista de Peças:\n','boolean_value': False }
+        self.er29 = {'description': ':lady_beetle: Error 29: Bloco de Legenda com descrição no TIT-2 ou TIT-4.','boolean_value': False }
         self.al01 = {'description': ':lady_beetle: Alert 01: Quantidade de peças diverge do número de Marcas no desenho:\n','boolean_value': False }
 
     # self.erXX = {'description': ':lady_beetle: Error XX: XXXXXXXXX','boolean_value': False }
