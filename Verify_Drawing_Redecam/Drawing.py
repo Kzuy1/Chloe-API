cc8836f0741c176282e76384f4b0005e97531822
Kzuy1 <leoczuy@gmail.com>
2026-08-13 20:48:35 -0300

    feat: Update message error description in `Error-39`

--- Verify_Drawing_Redecam/Drawing.py
+++ Verify_Drawing_Redecam/Drawing.py
@@ -735,15 +735,13 @@ class Drawing:
 
                 # article_by_block = self.db_articoli.normalize_article(article, block["QTY"]["value"], block_type)
 
-                # for attribute, value in article_by_block.items():
+                        self.error_drawing.er39["boolean_value"] = True
 
-                #     if str(block[attribute]["value"]).strip() != str(value).strip():
-                #         self.error_drawing.er39["boolean_value"] = True
+                        self.error_drawing.er39["description"] += (
+                            f"\t\t\t{block['MARK']['value']} - "
+                            f"{block['DESCL1']['value']}\n"
+                            f"\t\t\t\t{attribute}: "
+                            f"Desenho=`{block[attribute]['value']}` | "
+                            f"DB=`{value}`\n"
+                        )
 
-                #         self.error_drawing.er39["description"] += (
-                #             f"\t\t\t{block['MARK']['value']} - "
-                #             f"{block['DESCL1']['value']}\n"
-                #             f"\t\t\t\t{attribute}: "
-                #             f"Desenho='{block[attribute]['value']}' | "
-                #             f"DB='{value}'\n"
-                #         )
